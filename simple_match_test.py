9bd707e50ff7204de7d5623ea49e404274da62db
egorrozinskiy <egor.rozinskiy@mail.ru>
2016-04-25 11:04:17 +0400

    Update simple_match_test.py

--- simple_match_test.py
+++ simple_match_test.py
@@ -1,70 +1,54 @@
-#!/usr/bin/env python3
-
-# Для каждого регулярного выражения, которое требуется написать,
-# указаны строки, соответствующие этому выражению (они отмечены знаком +),
-# а также строки, не соответствующие этому выражению (отмечены знаком -)
-
-# + a
-# + ab
-# - b
-# - ba
-REGEXP_1 = '^[a]{1}[b]?$'
-
-# + aab
-# + abb
-# + acb
-# - ab
-# - aabc
-REGEXP_2 = '^[a]{1}[abc]{1}[b]{1}$'
-
-# + sofia.mp3
-# + sofia.mp4
-# - sofia.mp7
-# - sofia.mp34
-REGEXP_3 = '^sofia\.mp[34]{1}$'
-
-# + taverna
-# + versus
-# + vera
-# + zveri
-# - zver
-REGEXP_4 = '^.*[^r]$'
-
-# - a
-# - aa
-# + aaa
-# - aaaa
-# - b
-# - bb
-# + bbb
-# - bbbb
-# - ccc
-REGEXP_5 = '^[ab]{3}$'
-
-# - Ok
-# - OkOk
-# + OkOkOk
-# - OkOkOkOk
-# - ab
-# - abab
-# + ababab
-# - abababab
-REGEXP_6 = '^[Okab]{6}$'
-
-# - aaa
-# - aaa aaa
-# + aaa Aaa aaa
-# + aaa aaa Aaa
-# + Aaa aaa aaa
-# - A
-# - aaa A aaa
-REGEXP_7 = '[Aa]{3} {1}[Aa]{3} {1}[Aa]{3}'
-
-# + abc
-# + abc03
-# + a-b-c-3
-# + a.b.c.0
-# - Aabc
-# - abc1
-# - #abc
-REGEXP_8 = '^[abc.03-]{3,7}$'
++#!/usr/bin/env python3
++
++from regexp_test import regexp_test, MATCH
++import simple_match
++
++import unittest
++
++@regexp_test(simple_match)
++class SimpleMatchTest(unittest.TestCase):
++    TEST_DATA = {
++        'REGEXP_1': (    # название тестируемого регулярного выражения
++            MATCH,       # тип тестируемого метода — MATCH для этого файла
++            ['a', 'ab'], # список строк, соответствующих шаблону
++            ['b', 'ba']  # список строк, не соответствующих шаблону
++        ),
++        'REGEXP_2': (
++            MATCH,
++            ['aab', 'abb', 'acb'],
++            ['ab', 'aabc']
++        ),
++        'REGEXP_3': (
++            MATCH,
++            ['sofia.mp3', 'sofia.mp4'],
++            ['sofia.mp7', 'sofia.mp34']
++        ),
++        'REGEXP_4': (
++            MATCH,
++            ['taverna', 'versus', 'vera', 'zveri'],
++            ['zver']
++        ),
++        'REGEXP_5': (
++            MATCH,
++            ['aaa', 'bbb'],
++            ['a', 'aa', 'aaaa', 'b', 'bb', 'bbbb']
++        ),
++        'REGEXP_6': (
++            MATCH,
++            ['OkOkOk', 'ababab'],
++            ['Ok', 'OkOk', 'OkOkOkOk', 'ab', 'abab', 'abababab']
++        ),
++        'REGEXP_7': (
++            MATCH,
++            ['aaa Aaa aaa', 'aaa aaa Aaa', 'Aaa aaa aaa'],
++            ['aaa', 'aaa aaa', 'A', 'aaa A aaa']
++        ),
++        'REGEXP_8': (
++            MATCH,
++            ['abc', 'abc03', 'a-b-c-3', 'a.b.c.0'],
++            ['Aabc', 'abc1', '#abc']
++        )
++    }
++
++if __name__ == '__main__':
++    unittest.main()
